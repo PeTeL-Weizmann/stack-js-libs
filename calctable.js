bce1bd5c580ceef732f1a32896eb3077c5f74863
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2023-11-30 09:16:57 +0200

    Create calctable.js

---
+++ calctable.js
@@ -0,0 +1,50 @@
+<script src="https://bossanova.uk/jspreadsheet/v4/jexcel.js"></script>
+<link rel="stylesheet" href="https://bossanova.uk/jspreadsheet/v4/jexcel.css" type="text/css" />
+ <script src="https://jsuites.net/v4/jsuites.js"></script>
+<link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
+
+
+
+
+ [[jsxgraph input-ref-ans2='ans2Ref' width="0px" height="0px"]]
+
+//hide or show the fields for design
+if ({#design#} == 1) { document.getElementById("data{#rqm#}" ).style.display = "block" }
+
+ var dataInput = document.getElementById(ans2Ref);
+
+var zData=["","","","","","","","","","","","","","",""];
+
+var data = [zData.slice(0,{#Titles#}.length)];
+
+if (dataInput.value!=( dataInput.value != '')) {data = JSON.parse(dataInput.value)} else {dataInput.value=JSON.stringify(data)};
+
+var widths=[];
+for (let i=0;i<{#Titles#}.length;i++){widths[i]=120};
+
+var table=jspreadsheet(document.getElementById('spreadsheet{#rqm#}'), {
+  data:data,
+  colHeaders:{#Titles#},
+ colWidths: widths,
+    columns: [
+        { type: 'dropdown',   source:[  'יחס מולים',  'מסה',  'מסה מולרית',  'מספר אלקטרונים', 'מספר חלקיקים', 'מספר מולים', 'נפח גז', 'נפח מולרי', 'נפח תמיסה', 'ריכוז' ]  },
+        { type: 'dropdown',   source:["gr","gr/mol","kg","kJ","ℓiter","ℓiter/mol","M","ml","mol","mol/ℓiter","atoms","molecules"  ]   },
+     
+     ]
+});
+
+table.onafterchanges = function() {dataInput.value=JSON.stringify(table.getData())};
+
+if ({#hint_enable#}==1){
+var btn = document.createElement("BUTTON");  //<button> element
+var t = document.createTextNode("hint"); // Create a text node
+btn.appendChild(t);   
+
+btn.onclick = function(e){
+    e.preventDefault(); 
+    table.setData({#hintdata#})
+};  
+document.getElementById("myView{#rqm#}").appendChild(btn);
+}
+[[/jsxgraph]]
+
