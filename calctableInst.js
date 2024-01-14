aa624e934905cc7d3900e8fdb60b8b26c472218a
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-01-14 10:19:41 +0200

    Update calctableInst.js

--- calctableInst.js
+++ calctableInst.js
@@ -22,7 +22,12 @@
 }
  </style>
 
-  
+<script type="text/javascript">
+ const button = document.querySelector('button[id$="-1_-submit"]');
+ button.addEventListener('click', (event) => {
+ checkAnswer[{#rqm#}]();
+});
+</script>
    
   <div style="display:none;">\(x^2\)
  [[jsxgraph input-ref-ans2='ans2Ref' input-ref-ans3='ans3Ref' width="0px" height="0px"]]
