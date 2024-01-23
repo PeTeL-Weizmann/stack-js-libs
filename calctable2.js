c45ba4eaf3244a149bbcc58ebf3e5649c637ad3e
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-01-23 23:22:55 +0200

    Update calctable2.js

--- calctable2.js
+++ calctable2.js
@@ -1,4 +1,6 @@
-<script src="https://rawcdn.githack.com/raedshorrosh/calc/8146613d0a409575be7514c420a69d6e67416f4b/jexcel.js"></script>
+ var S=function(value,n){
+     var m=value.toExponential(n);
+  return m;<script src="https://rawcdn.githack.com/raedshorrosh/calc/8146613d0a409575be7514c420a69d6e67416f4b/jexcel.js"></script>
 <link rel="stylesheet" href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" />
 <script src="https://rawcdn.githack.com/raedshorrosh/calc/e2314623eb24ac6307538026626463d67c90e562/jsuites.js"></script>
 <link rel="stylesheet" href="https://rawcdn.githack.com/raedshorrosh/calc/e2314623eb24ac6307538026626463d67c90e562/jsuites.css" />
@@ -55,7 +57,7 @@ var widths=[180,120,120];
  var S=function(value,n){
      var m=value.toExponential(n);
   return m;
- 
+ };
 for (let i=3;i<{#Titles#}.length;i++){widths[i]=120};
 var toolbar=[
         {
