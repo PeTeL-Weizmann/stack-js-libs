6732024cb3159f3e77e5127b1a597ac6652ef4de
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-07-31 15:35:48 +0300

    Update instTable3.js

--- instTable3.js
+++ instTable3.js
@@ -5,7 +5,7 @@
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
-'ver 7'
+'ver 8'
 
 
  <div style="display: flex; justify-content: center;" width="100%">
