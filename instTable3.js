e7ae7f36898a0bd67c21ac819f707ec480ca7dd1
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-07-31 13:41:11 +0300

    Update instTable3.js

--- instTable3.js
+++ instTable3.js
@@ -1,11 +1,11 @@
-[[iframe width={#width#} height={#height#}]]
+[[iframe width={@width@} height={@height@}]]
 [[script src="https://rawcdn.githack.com/raedshorrosh/calc/8146613d0a409575be7514c420a69d6e67416f4b/jexcel.js"/]]
 [[script src="https://rawcdn.githack.com/raedshorrosh/calc/e2314623eb24ac6307538026626463d67c90e562/jsuites.js"/]]
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/e2314623eb24ac6307538026626463d67c90e562/jsuites.css" type="text/css" /]]
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
-'ver 3'
+'ver 4'
 
 
  <div style="display: flex; justify-content: center;" width="100%">
