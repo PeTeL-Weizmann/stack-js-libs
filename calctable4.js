0350075f04888206c4f68d093042eda77e2c4aec
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-02 12:00:20 +0300

    Update calctable4.js

--- calctable4.js
+++ calctable4.js
@@ -5,7 +5,7 @@
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
-ver 0.4
+ver 0.5
  <div style="display: flex; justify-content: center;" width="100%">
    <div id="spreadsheet" dir="ltr" ></div>
    <div id="myView" style="display:none;" ></div>
@@ -18,7 +18,7 @@ ver 0.4
     font-size:16 px;
 }
  </style>
- 
+ <p style=display:anone>`x^2`,`x/y', \(\small C_6H_{12}O_{6(s)}\)</p>
    
 [[script type="module"]]
 
