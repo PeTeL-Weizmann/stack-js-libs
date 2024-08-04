f98fa3214721cfa62d5e057ee0ff557bcb54dd47
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-04 16:38:54 +0300

    Update tttt.js

--- tttt.js
+++ tttt.js
@@ -5,13 +5,13 @@
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
- ver 1.11-9
+ ver 1.11-11
  <p style=display:none>`x^2`,`x/y`, \(\small C_6H_{12}O_{6(s)}\)</p>
  
  <div style="display: flex; justify-content: center; width:100%; font-size:{@fontsize@}">
-   <div id="spreadsheet" dir="ltr" ></div>
-   <div id="myView" style="display:none;" ></div>
- </div>
+  <div id="myView" style="display:none;" ></div>
+  <div id="spreadsheet" dir="ltr" ></div>
+  </div>
 
                                  
 
@@ -166,7 +166,20 @@ nestedHeaders:nested,
    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 table.onbeforechange= function(instance, cell, x, y, value){if (readonly) {cell.classList.add('readonly')}};
      
-      //create the hint button
+   
+
+
+ 
+stack_js.get_content("contentCT{#rqm#}").then((content) => {
+ console.log(' the content in the span is ',content);
+if (content !== null) {
+   console.log('count.value before',count.value);
+   count.value=JSON.srtingify([3]); count.dispatchEvent(new Event('change'));
+     console.log('counter value is now ',count.value);
+  
+}});  
+
+  //create the hint button
       var btn = document.createElement("button");  //<button> element
       var t = document.createTextNode("hint"); // Create a text node
       btn.appendChild(t);   
@@ -180,24 +193,14 @@ table.onbeforechange= function(instance, cell, x, y, value){if (readonly) {cell.
        hint_el.appendChild(btn);
      btn.disabled=true; 
                console.log("the count value is ",count.value);
-              if (count.value!=='' )
-              {
+            try {               
                  let value=JSON.parse(count.value);
                   showHint=(value[0]==3);
                   console.log("show the button is ",showHint);
                  if ( ({#hint_enable#}==1) || showHint) {hint_el.style.display = "block";btn.disabled = false;}        
               }
 
-
- 
-stack_js.get_content("contentCT{#rqm#}").then((content) => {
- console.log(' the content in the span is ',content);
-if (content !== null) {
-   console.log('count.value before',count.value);
-   count.value=JSON.srtingify([3]); count.dispatchEvent(new Event('change'));
-     console.log('counter value is now ',count.value);
-  
-}});   
+              catch(err) {console.log("nothing in yet");}
  });
 [[/script]]
 </div>
