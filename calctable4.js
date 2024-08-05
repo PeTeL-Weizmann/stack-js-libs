e93e5bdc0280093ac62e500b4f044860d3e1d65c
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-05 12:22:15 +0300

    Update calctable4.js

--- calctable4.js
+++ calctable4.js
@@ -5,7 +5,7 @@
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
-[[comment]] ver 1.11 [[/comment]]
+ver 1.12
  <p style=display:none>`x^2`,`x/y`, \(\small C_6H_{12}O_{6(s)}\)</p>
  
  <div style="display: flex; justify-content: center; width:100%; font-size:{@fontsize@}">
@@ -157,6 +157,7 @@ nestedHeaders:nested,
    var isAttemptMode = window.location.href.indexOf('attempt.php') !== -1;
    if (isAttemptMode && readonly) cell.classList.add('readonly');                                    
    dataInput.value=JSON.stringify(instance.jspreadsheet.getData());dataInput.dispatchEvent(new Event('change'));
+   MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     },    
   columnSorting:false,
 });
